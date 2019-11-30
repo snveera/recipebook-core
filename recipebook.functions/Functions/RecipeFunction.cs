e74a1c10caeedcb3c1b2d0b44661d9e966d50160
Josh Rolstad <jrolstad@gmail.com>
2019-11-30 15:10:08 -0800

    Implementing function recipes

---
+++ recipebook.functions/Functions/RecipeFunction.cs
@@ -0,0 +1,74 @@
+﻿
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using recipebook.core.Managers;
+using recipebook.core.Models;
+
+namespace recipebook.functions.Functions
+{
+    public class RecipeFunction
+    {
+        private readonly RecipeManager _manager;
+
+        public RecipeFunction(RecipeManager manager)
+        {
+            _manager = manager;
+        }
+
+        [FunctionName("api-recipe-get-all")]
+        public async Task<IActionResult> GetAll(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "recipe")] HttpRequest req,
+            ILogger log)
+        {
+            var result = _manager.Get();
+
+            return new OkObjectResult(result);
+        }
+
+        [FunctionName("api-recipe-get-byid")]
+        public async Task<IActionResult> GetItem(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "recipe/{id}")] HttpRequest req,
+            ILogger log,
+            string id)
+        {
+            var result = _manager.Get(id);
+
+            return new OkObjectResult(result);
+        }
+
+        [FunctionName("api-recipe-create")]
+        public async Task<IActionResult> Create(
+           [HttpTrigger(AuthorizationLevel.Function, "post", Route = "recipe")] HttpRequest req,
+           ILogger log,
+           string id)
+        {
+
+            var data = await req.ReadAsStringAsync();
+            var recipeData = JsonConvert.DeserializeObject<Recipe>(data);
+
+            var result = _manager.Create(recipeData);
+
+            return new OkObjectResult(result);
+        }
+
+        [FunctionName("api-recipe-update")]
+        public async Task<IActionResult> Update(
+          [HttpTrigger(AuthorizationLevel.Function, "put", Route = "recipe")] HttpRequest req,
+          ILogger log,
+          string id)
+        {
+
+            var data = await req.ReadAsStringAsync();
+            var recipeData = JsonConvert.DeserializeObject<Recipe>(data);
+
+            var result = _manager.Update(recipeData);
+
+            return new OkObjectResult(result);
+        }
+    }
+}
