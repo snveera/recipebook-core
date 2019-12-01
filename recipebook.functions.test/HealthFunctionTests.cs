33f8cfb2e1c73f40ccf0c51d64708cfdff1a3183
Josh Rolstad <jrolstad@gmail.com>
2019-11-30 21:25:04 -0800

    Adding unit tests around the health function

---
+++ recipebook.functions.test/HealthFunctionTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using recipebook.core.Models;
+using recipebook.functions.Functions;
+using recipebook.functions.test.TestUtility;
+using recipebook.functions.test.TestUtility.Extensions;
+using Xunit;
+
+namespace recipebook.functions.test
+{
+    public class HealthFunctionTests
+    {
+        [Fact]
+        public async Task Run_NoInputs_ReturnsApplicationData()
+        {
+            // Given
+            var root = TestCompositionRoot.Create();
+
+            var api = root.Get<HealthFunction>();
+
+            // When
+            var result = await api.Run(root.GetRequest(), root.CoreLogger());
+
+            // Then
+            var healthResult = result.AssertIsOkResultWithValue<ApplicationHealth>();
+
+            Assert.True(!string.IsNullOrWhiteSpace(healthResult.Version));
+            Assert.InRange(healthResult.CurrentDateTime, DateTime.Today, DateTime.Today.AddDays(1));
+
+            Assert.True(healthResult.Status);
+            Assert.NotEmpty(healthResult.DependencyStatus);
+        }
+    }
+}
