fc9d03f99b3f6b3fc59fdbfc0f61c9aea3f5b788
Josh Rolstad <jrolstad@gmail.com>
2019-12-01 21:32:27 -0800

    Updating to use Azure Function

--- recipebook.blazorclient/Application/Services/ConfigurationService.cs
+++ recipebook.blazorclient/Application/Services/ConfigurationService.cs
@@ -9,7 +9,7 @@
     {
         public string UserApiUrl()
         {
-            return "http://localhost:7071/api/user";
+            return @"https://recipebook-functions.azurewebsites.net/api/user?code=vSvDxrFqS0WfpGGkpDBo52EoSZUDRzXKLIMpK5l5cO2MIwCA80ZWfg==";
         }
     }
 }
