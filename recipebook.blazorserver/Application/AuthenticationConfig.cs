9e05b70d0e5e4cce81e3a3acd9b5df01a1fb72e3
Josh Rolstad <jorolsta@microsoft.com>
2019-12-12 14:52:02 -0800

    Abstracting out the configuration so it's more coherent

---
+++ recipebook.blazorserver/Application/AuthenticationConfig.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.AzureAD.UI;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace recipebook.blazorserver.Application
+{
+    public class AuthenticationConfig
+    {
+        public static void Configure(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddAuthentication(AzureADDefaults.AuthenticationScheme)
+                .AddAzureAD(options => configuration.Bind("AzureAd", options));
+        }
+
+        public static void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseAuthentication();
+            app.UseAuthorization();
+        }
+    }
+}
---
+++ recipebook.blazorserver/Application/BlazorConfig.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace recipebook.blazorserver.Application
+{
+    public class BlazorConfig
+    {
+        public static void Configure(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
+        }
+
+        public static void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapBlazorHub();
+                endpoints.MapFallbackToPage("/_Host");
+            });
+        }
+    }
+}
