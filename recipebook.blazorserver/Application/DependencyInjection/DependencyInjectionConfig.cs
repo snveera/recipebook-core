25b716651206ed3d76bffeca2402c5ebeb195b6a
Josh Rolstad <jorolsta@microsoft.com>
2019-12-12 13:07:34 -0800

    Stubbing out server side

---
+++ recipebook.blazorserver/Application/DependencyInjection/DependencyInjectionConfig.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using recipebook.blazor.core.Services;
+using recipebook.blazor.core.ViewModels;
+
+namespace recipebook.blazorserver.Application.DependencyInjection
+{
+    public class DependencyInjectionConfig
+    {
+        public static void Configure(IServiceCollection services)
+        {
+            
+            services.AddTransient<IConfigurationService, ConfigurationService>();
+            services.AddTransient<IUserService,UserService>();
+
+            services.AddTransient<RecipeIndexViewModel>();
+            services.AddTransient<RecipeDetailViewModel>();
+            services.AddTransient<UserViewModel>();
+
+            services.AddTransient<CategoryService>();
+            services.AddSingleton<ICategoryService, CachedCategoryService>();
+            
+            services.AddTransient<IRecipeService,RecipeService>();
+        }
+    }
+}
