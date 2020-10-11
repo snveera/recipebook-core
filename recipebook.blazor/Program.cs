8a93a2bf93039f12eb3e525647c573fbce46a100
Josh Rolstad <jorolsta@microsoft.com>
2020-10-11 15:51:30 -0700

    Stubbing out authorization

--- recipebook.blazor/Program.cs
+++ recipebook.blazor/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 
 namespace recipebook.blazor
 {
@@ -16,10 +17,33 @@ namespace recipebook.blazor
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
+            builder.Logging.SetMinimumLevel(LogLevel.Warning);
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            RegisterDefaultHttpClient(builder);
+            RegisterGraphHttpClient(builder);
+
+            builder.Services.AddMsalAuthentication(options =>
+            {
+                builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
+                options.ProviderOptions.DefaultAccessTokenScopes.Add("https://graph.microsoft.com/User.Read");
+            });
 
             await builder.Build().RunAsync();
+        }
+
+        private static void RegisterDefaultHttpClient(WebAssemblyHostBuilder builder)
+        {
+            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+        }
+
+        private static void RegisterGraphHttpClient(WebAssemblyHostBuilder builder)
+        {
+            builder.Services.AddHttpClient("GraphApi", client =>
+                client.BaseAddress = new Uri("https://graph.microsoft.com"))
+                    .AddHttpMessageHandler(sp => sp.GetRequiredService<AuthorizationMessageHandler>()
+                    .ConfigureHandler(new[] { "https://graph.microsoft.com" }, new[] { "https://graph.microsoft.com/User.Read" }));
+
+
         }
     }
 }
